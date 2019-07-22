8c706a1a915aaf5ce1d76ea7223e833e56352d88
wilsongao <303423912@qq.com>
2019-07-22 13:03:00 +0800

    创建了header部分初步的解构

--- sites/all/themes/zguyun/templates/page.tpl.php
+++ sites/all/themes/zguyun/templates/page.tpl.php
@@ -72,3 +72,46 @@
  * @ingroup themeable
  */
 ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-1">
+        <h1>
+            LOGO
+        </h1>
+        </div>
+        <div class="col-lg-7">
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="container-fluid"> 
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">菜鸟教程</a>
+                </div>
+                <div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">iOS</a></li>
+                        <li><a href="#">SVN</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                Java 
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">jmeter</a></li>
+                                <li><a href="#">EJB</a></li>
+                                <li><a href="#">Jasper Report</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">分离的链接</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">另一个分离的链接</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+        </div> 
+        <div class="col-lg-2">
+            搜索 | 购物车 | 登录
+        </div> 
+    </div>
+</div>
